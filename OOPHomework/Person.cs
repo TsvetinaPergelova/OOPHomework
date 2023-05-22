46fce934f016be58b802c86d772a79fe13a93112
PC <PC@DESKTOP-MF4MP61>
2023-05-22 14:48:26 +0300

    Add project files.

---
+++ OOPHomework/Person.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPHomework
+{
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class FootballPlayer : Person
+    {
+        public int Number { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class Goalkeeper : FootballPlayer
+    {
+        public Goalkeeper(string name, int number, int age, double height)
+        {
+            Name = name;
+            Number = number;
+            Age = age;
+            Height = height;
+        }
+    }
+
+    public class Defender : FootballPlayer
+    {
+        public Defender(string name, int number, int age, double height)
+        {
+            Name = name;
+            Number = number;
+            Age = age;
+            Height = height;
+        }
+    }
+
+    public class Midfield : FootballPlayer
+    {
+        public Midfield(string name, int number, int age, double height)
+        {
+            Name = name;
+            Number = number;
+            Age = age;
+            Height = height;
+        }
+    }
+
+    public class Striker : FootballPlayer
+    {
+        public Striker(string name, int number, int age, double height)
+        {
+            Name = name;
+            Number = number;
+            Age = age;
+            Height = height;
+        }
+    }
+
+    public class Coach : Person
+    {
+        public Coach(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    public class Referee : Person
+    {
+        public Referee(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    public class Team
+    {
+        public Coach Coach { get; set; }
+        public FootballPlayer[] Players { get; set; }
+
+        public double CalculateAverageAge()
+        {
+            int totalAge = 0;
+            foreach (var player in Players)
+            {
+                totalAge += player.Age;
+            }
+            return (double)totalAge / Players.Length;
+        }
+    }
+
+    public class Game
+    {
+        public Team Team1 { get; set; }
+        public Team Team2 { get; set; }
+        public Referee Referee { get; set; }
+        public Person[] AssistantReferees { get; set; }
+        public string Result { get; set; }
+        public Team Winner { get; set; }
+
+        public void PrintResult()
+        {
+            Console.WriteLine("Game Result: " + Result);
+        }
+    }
+}
+
+    
+
---
+++ OOPHomework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOPHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating players
+            FootballPlayer player1 = new Goalkeeper("John", 1, 25, 180);
+            FootballPlayer player2 = new Defender("Mike", 5, 28, 185);
+            FootballPlayer player3 = new Midfield("David", 10, 30, 175);
+            FootballPlayer player4 = new Striker("Chris", 9, 27, 182);
+            FootballPlayer player5 = new Midfield("George", 2, 35, 170);
+            FootballPlayer player6 = new Defender("Ivan", 4, 77, 200);
+
+            // Creating coach
+            Coach coach = new Coach("Alex", 45);
+
+            // Creating team
+            Team team = new Team();
+            team.Coach = coach;
+            team.Players = new FootballPlayer[] { player1, player2, player3, player4 };
+            double averageAge = team.CalculateAverageAge();
+
+            // Creating referee
+            Referee referee = new Referee("Mark", 40);
+
+            // Creating game
+            Game game = new Game();
+            game.Team1 = team;
+            game.Referee = referee;
+        }
+    }
+}
